10a565cd740b2c4b2c4b97cab362b8212c15045d
you@example.com <you@example.com>
2021-07-29 10:46:05 +1200

    axios api

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
+const axios = require('axios');
 
 
 
 
 
 
+// axios.get('https://api.coingecko.com/api/v3/ping')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
 
-axios.post('/login', {
-    firstName: 'Finn',
-    lastName: 'Williams'
-  })
-  .then((response) => {
-    console.log(response);
-  }, (error) => {
-    console.log(error);
-  });
+const applicationId = `6100d5c19ca930001fc0d7e6`
+const resourceUrl = `https://api.knack.com/v1/applications/${applicationId}`
+
+// ~server/api/application.js
+// your-router-function-here ...
+
+axios.get(resourceUrl)
+.then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
